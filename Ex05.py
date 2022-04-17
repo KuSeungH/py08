4e25087e56e80068a5ca9a8f24ffd98046d598c9
KuSeungH <98626004+KuSeungH@users.noreply.github.com>
2022-04-17 10:23:15 +0900

    Add files via upload

---
+++ Ex05.py
@@ -0,0 +1,11 @@
+# ex05.py
+
+from phonebook import phonebook
+
+p1 = phonebook('이지은', '01011111111')
+p2 = phonebook('홍진호', '010-2222-2222')
+p3 = phonebook('짱구', '1234')
+
+print(p1)
+print(p2)
+print(p3)
